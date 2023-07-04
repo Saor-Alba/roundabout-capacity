85e8b3f8f320364020bd1ee707e04bd63f9c2977
SPENCE Angus <aspence1@systra.info>
2023-07-04 11:04:46 +0100

    alts

--- execution.py
+++ execution.py
@@ -4,8 +4,8 @@ from modules.calibration import Calibration, Calibrations, Calibration_Target
 if __name__ == "__main__":
     traffic_profiler = Profiler(profile_type=Profiles.ONEHOUR)
     model_calibration = Calibration(
-        calibration_type=Calibrations.INTERCEPT,
-        calibration_method=Calibration_Target.PCU,
+        calibration_type = Calibrations.INTERCEPT,
+        calibration_method = Calibration_Target.PCU,
         rfc=[],
         calibration_variables=[],
         calibration_targets=[]
