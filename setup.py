528fd729c53b6560fd7d07ca12aa9d5e381dd105
SPENCE Angus <aspence1@systra.info>
2023-07-06 16:26:06 +0100

    setuptools

---
+++ setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name='roundabout_capacity',
+    version='0.0.1',
+    author='Angus Spence',
+    packages=['modules'],
+    install_requires=['numpy']
+)
