77b0223d5c864220a38f687f522589174e7f10b6
angus-spence <gusmalcolmspence@gmail.com>
2023-07-24 13:32:44 +0100

    updates to v1.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='roundabout_capacity',
-    version='0.0.1',
+    version='1.0.0',
     author='Angus Spence',
     packages=['modules_python', 'modules_vba'],
     install_requires=['numpy', 'xlwings']
