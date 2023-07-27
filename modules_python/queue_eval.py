dc3e1d164ba37531885bfc7b288bfa2d53887f91
angus-spence <gusmalcolmspence@gmail.com>
2023-07-27 15:04:44 +0100

    ANN for queue evaluation

--- modules_python/queue_eval.py
+++ modules_python/queue_eval.py
@@ -5,36 +5,60 @@ import numpy as np
 import torch
 from torch.autograd import Variable
 
-@dataclass
+LEARNING_RATE = 0.001
+EPOCHS = 100
+
 class Queue_Model(torch.nn.Module):
-    half_lane_width: list
-    entry_width: list
-    effective_flare_length: list
-    entry_radius: list
-    inscribed_circle_diameter: list
-    conflict_angle: list
-    entry_flow: list
-    circulatory_flow: list
-    observed_queue: list
-    period_total_minutes: int
-    period_interval_minutes: int
-    
-    def __post_init__(self) -> None:
-        try: self.x = np.array([getattr(self, vars(Queue_Model)['__match_args__'][:-2][i]) for i in range(len(vars(Queue_Model)['__match_args__'][:-2]))])
-        except ValueError: print(f'Inputs do not have uniform length')
-        self.y = np.array(self.observed_queue)
+    def __init__(self, half_lane_width, entry_width, effective_flare_length, entry_radius,
+                 inscribed_circle_diameter, conflict_angle, entry_flow, circulatory_flow,
+                 observed_queue, period_total_minutes, period_interval_minutes):
+        self.half_lane_width: list = half_lane_width
+        self.entry_width: list = entry_width
+        self.effective_flare_length: list = effective_flare_length 
+        self.entry_radius: list = entry_radius
+        self.inscribed_circle_diameter: list = inscribed_circle_diameter
+        self.conflict_angle: list = conflict_angle
+        self.entry_flow: list = entry_flow
+        self.circulatory_flow: list = circulatory_flow
+        self.observed_queue: list = observed_queue 
+        self.period_total_minutes: int = period_total_minutes
+        self.period_interval_minutes: int = period_interval_minutes#
+        
+        self.x = Variable(torch.from_numpy(np.array([
+            self.half_lane_width,
+            self.entry_width,
+            self.effective_flare_length,
+            self.entry_radius,
+            self.inscribed_circle_diameter,
+            self.conflict_angle,
+            self.entry_flow,
+            self.circulatory_flow,
+            self.observed_queue])).type(torch.float32))
+        self.y = Variable(torch.from_numpy(np.array(self.observed_queue)))
         self.input_size = len(self.x)
         self.output_size = len(self.y)
-        super(Queue_Model, self).__init__()
-        self.linear = torch.nn.Linear(self.input_size, self.output_size)    
-        
+        self.obs = len(self.half_lane_width)
+        print(self.input_size)
+
+        super().__init__()
+        self.flatten = torch.nn.Flatten()
+        self.linear_relu_stack = torch.nn.Sequential(
+            torch.nn.Linear(9, 200),
+            torch.nn.ReLU(),
+            torch.nn.Linear(200, 128),
+            torch.nn.ReLU(),
+            torch.nn.Linear(128, 1)
+        )
+    
+    def __post_init__(self) -> None:
         if len(self.circulatory_flow) == len(self.observed_queue): return None
         elif len(self.observed_queue) % len(self.circulatory_flow) == 0: return self.align()
         else: raise ValueError(f'Dimension of Circulatory Flow and Obsevered Flows are not equal and cannot be resolved: OQ({len(self.observed_queue)}) / CF({len(self.circulatory_flow)}) != int')
 
     def forward(self, x):
-        out = self.linear(x)
-        return out
+        x = self.flatten(x)
+        logits = self.linear_relu_stack(x)
+        return logits
 
     def factor(self) -> float:
         return int(self.period_total_minutes / self.period_interval_minutes)
@@ -43,39 +67,29 @@ class Queue_Model(torch.nn.Module):
         chunks = [self.observed_queue[x:x + (len(self.observed_queue / self.factor()))] for x in range(self.factor())]
         self.observed_queue = [round(sum(chunks[i]) / len(chunks[i]), 2) for i in range(len(chunks))]
 
-    def evaluate(self):
-        learning_rate = 0.001
-        epochs = 100
-        
-        model = Queue_Model
-        criterion = torch.nn.MSELoss()
-        optimiser = torch.optim.SGD(model.parameters(), lr=learning_rate)
-
-        for epoch in range(epochs):
-            x = Variable(torch.tensor(self.x))
-            y = Variable(torch.tensor(self.y))
-
-            optimiser.zero_grad()
-            outputs = model(x)
-            
-            loss = criterion(outputs, y)
-            loss.backward()
-            optimiser.step()
-            print(f'Epoch: {epoch} - Loss {loss.item()}')
-
 geom = [
-    np.random.randint(3, 5, size=200),
-    np.random.randint(3, 5, size=200),
-    np.random.randint(3, 5, size=200),
-    np.random.randint(3, 5, size=200),
-    np.random.randint(3, 5, size=200),
-    np.random.randint(3, 5, size=200),
-    np.random.randint(3, 5, size=200)
+    np.random.randint(3, 6, size=200),
+    np.random.randint(5, 9, size=200),
+    np.random.randint(20, 30, size=200),
+    np.random.randint(32, 70, size=200),
+    np.random.randint(20, 30, size=200),
+    np.random.randint(100, 500, size=200),
+    np.random.randint(100, 400, size=200)
 ]
-
-
-
 
 if __name__ == "__main__":
     q = Queue_Model(*geom, np.random.randint(0, 20, size=200), np.random.randint(0, 12, size=200), 60, 15)
-    q.evaluate()
+    criterion = torch.nn.MSELoss()
+    optimiser = torch.optim.SGD(q.parameters(), lr=LEARNING_RATE)
+
+    for epoch in range(EPOCHS):
+        x = Variable(torch.tensor(q.x))
+        y = Variable(torch.tensor(q.y))
+
+        optimiser.zero_grad()
+        outputs = q(x)
+        
+        loss = criterion(outputs, y)
+        loss.backward()
+        optimiser.step()
+        print(f'Epoch: {epoch} - Loss {loss.item()}')
